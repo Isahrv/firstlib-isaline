d8425f1e83f51aee07cf767625a6c9e3f2e43c68
Isaline <isalineherve@gmail.com>
2025-02-14 12:20:22 +0100

    ajout des nouvelles fonctions du td 5

---
+++ R/creer_commune.R
@@ -0,0 +1,11 @@
+#' Fonction pour réer un objet de classe "commune"
+#'
+#' @param df Un dataframe avec les informations d'une commune
+#' @return Un objet de classe "commune"
+#' @export
+creer_commune <- function(df) {
+  # Cette fonction prend un dataframe correspondant au schéma de validate_schema(), en entrée et ajoute la classe "commune" au dataframe
+  validate_schema(df)
+  class(df) <- c("commune", class(df))
+  return(df)
+}
---
+++ R/creer_departement.R
@@ -0,0 +1,11 @@
+#' Fonction pour réer un objet de classe "departement"
+#'
+#' @param df Un dataframe avec les informations d'un département
+#' @return Un objet de classe "departement"
+#' @export
+creer_departement <- function(df) {
+  # Cette fonction prend un dataframe correspondant au schéma de validate_schema(), en entrée et ajoute la classe "departement" au dataframe
+  validate_schema(df)
+  class(df) <- c("departement", class(df))
+  return(df)
+}
