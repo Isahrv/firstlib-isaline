bcaedb064dbb98045a5246da208028401ad26cbd
Isaline <isalineherve@gmail.com>
2025-02-22 23:59:55 +0100

    modifs bug summary.departement()

--- R/summary.departement.R
+++ R/summary.departement.R
@@ -2,49 +2,60 @@
 #' @method summary departement
 #' @description Résumé des informations pour un dataframe de classe "departement".
 #' @param x Un dataframe de classe "departement".
-#' @return Un résumé d'information sur un département, avec le libellé du département, le nombre de commune, le nombre d'élus, la distribution de l'âge des élus, des informations sur l'élu le/la plus agé.e et l'élu le/la plus jeune, leur commune, le nom de la commune avec la moyenne d'âge la plus faible et avec la moyenne d'âge la plus élevée, la distribution des âges pour ces communes
+#' @return Un résumé des informations sur un département.
 #' @import dplyr
 #' @import lubridate
 #' @export
 summary.departement <- function(x) {
-  # Cette fonction prend un dataframe de classe "departement", en entrée et retourne un résumé d'informations sur un département.
   print(paste("Nom du département :", unique(x$Libellé_du_département)))
   print(paste("Nombre de commune :", compter_nb_commune(x)))
   print(paste("Nombre d'élus dans le département :", compter_nb_elus(x)))
   print("Distribution de l'âge des élus du département :")
   print(calcul_distribution_age(x))
 
-  print("Élu(e) le/la plus âgé(e) du département :")
   elu_plus_age <- trouver_l_elu_le_plus_age(x)
+  cat("\nÉlu(e) le/la plus âgé(e) du département :\n")
   print(elu_plus_age)
-  print(paste("Commune de l'élu(e) le/la plus âgé(e) :", x$Libellé_de_la_commune[x$Nom_de_l_élu == elu_plus_age$Nom_de_l_élu & x$Prénom_de_l_élu == elu_plus_age$Prénom_de_l_élu]))
 
-  print("Élu(e) le/la plus jeune du département :")
+  commune_plus_age <- x |>
+    filter(Nom_de_l_élu == elu_plus_age$Nom_de_l_élu, Prénom_de_l_élu == elu_plus_age$Prénom_de_l_élu)|>
+    pull(Libellé_de_la_commune) |>
+    unique()
+
+  cat("Commune de l'élu(e) le/la plus âgé(e) :", commune_plus_age, "\n")
+
   elu_plus_jeune <- trouver_l_elu_le_plus_jeune(x)
+  cat("\nÉlu(e) le/la plus jeune du département :\n")
   print(elu_plus_jeune)
-  print(paste("Commune de l'élu(e) le/la plus jeune :", x$Libellé_de_la_commune[x$Nom_de_l_élu == elu_plus_jeune$Nom_de_l_élu & x$Prénom_de_l_élu == elu_plus_jeune$Prénom_de_l_élu]))
+
+  commune_plus_jeune <- x |>
+    filter(Nom_de_l_élu == elu_plus_jeune$Nom_de_l_élu, Prénom_de_l_élu == elu_plus_jeune$Prénom_de_l_élu)|>
+    pull(Libellé_de_la_commune) |>
+    unique()
+
+  cat("Commune de l'élu(e) le/la plus jeune :", commune_plus_jeune, "\n")
 
   age <- x |>
-    mutate(Date_de_naissance = dmy(Date_de_naissance)) |>
-    mutate(Age = as.numeric(difftime(Sys.Date(), Date_de_naissance, units = "days")) %/% 365)
+    mutate(Date_de_naissance = dmy(Date_de_naissance),
+           Age = as.numeric(difftime(Sys.Date(), Date_de_naissance, units = "days")) %/% 365)
 
   moyenne_age_par_commune <- age |>
     group_by(Libellé_de_la_commune) |>
     summarise(Moyenne_Age = mean(Age, na.rm = TRUE), .groups = "drop")
 
   commune_age_min <- moyenne_age_par_commune |>
-    slice(which.min(Moyenne_Age)) |>
+    slice_min(Moyenne_Age) |>
     pull(Libellé_de_la_commune)
 
   commune_age_max <- moyenne_age_par_commune |>
-    slice(which.max(Moyenne_Age)) |>
+    slice_max(Moyenne_Age) |>
     pull(Libellé_de_la_commune)
 
-  print(paste("Commune avec la moyenne d'âge la plus faible :", commune_age_min))
-  print("Distribution des âges pour cette commune :")
+  cat("\nCommune avec la moyenne d'âge la plus faible :", commune_age_min, "\n")
+  cat("Distribution des âges pour cette commune :\n")
   print(calcul_distribution_age(filter(x, Libellé_de_la_commune == commune_age_min)))
 
-  print(paste("Commune avec la moyenne d'âge la plus élevée :", commune_age_max))
-  print("Distribution des âges pour cette commune :")
+  cat("\nCommune avec la moyenne d'âge la plus élevée :", commune_age_max, "\n")
+  cat("Distribution des âges pour cette commune :\n")
   print(calcul_distribution_age(filter(x, Libellé_de_la_commune == commune_age_max)))
 }
